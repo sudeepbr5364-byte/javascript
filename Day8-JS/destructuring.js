78d6f10f2333a64664d40346885a334b56f40a0d
sudeep <sudeepbr5364@gmail.com>
2026-03-09 15:19:57 +0530

    Day8 - Destructuring Practice

---
+++ Day8-JS/destructuring.js
@@ -0,0 +1,52 @@
+/*Destructuring*/
+const person = {
+    name: "Sudeep",
+    age: 20,
+    city:"Bangalore"
+};
+const {name, age, city} = person;
+console.log(name);
+console.log(city);
+
+const student = {
+    name1: "Rahul",
+    marks: 85,
+    subject:"Math"
+};
+const {name1,marks} = student;
+console.log(name1);
+console.log(marks);
+
+/*Array Destructuring*/
+let numbers = [10,20,30];
+const [first,second] = numbers;
+console.log(first);
+console.log(second);
+
+let colors = ["red","blue","green"];
+const[c1,c2,c3] = colors;
+console.log(c1);
+console.log(c2);
+console.log(c3);
+
+let fruits = ["apple","banana","mango"];
+const [f1,f2,f3] = fruits;
+console.log(f1);
+console.log(f2);
+
+/*Destrcturing with renaming i.e renaming the variable name as somtimes we may need different variable name.*/
+const user = {
+    name:"Arjun",
+     age: 21
+};
+const {name:username,age: userage} = user;
+console.log(username);
+console.log(userage);
+
+const book = {
+    title: " Javascript Basics",
+    pages: 200
+};
+const { title:booktitle,pages:totalpages} = book;
+console.log(booktitle);
+console.log(totalpages);
